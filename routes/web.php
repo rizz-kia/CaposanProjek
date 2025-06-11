75929de20f4557dcdcb4503feddf7f8527576bd8
detina euis <detinaeuisnuraini@gmail.com>
2025-06-11 12:27:30 +0700

    hapus route duplikat

--- routes/web.php
+++ routes/web.php
@@ -13,28 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
-    return view('login');
-});
-<<<<<<< HEAD
+
+
 Route::get('/beranda-guru', function () {
     return view('beranda-guru');
-=======
+
+});
 Route::get('/beranda-santri', function () {
     return view('beranda-santri');
->>>>>>> 0282a7b1e5b81ce35af0a49049d547cd82430d9e
+
 });
 Route::get('/beranda-superadmin', function () {
     return view('beranda-superadmin');
 });
 
-Route::get('/beranda-guru', function () {
-    return view('beranda-guru');
-});
 
-Route::get('/beranda-superadmin', function () {
-    return view('beranda-superadmin');
-});
+
 
 use App\Http\Controllers\AuthController;
 
